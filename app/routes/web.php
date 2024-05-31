d8a60d71845dd7f0a1c0872b14545419300037ce
James <lawrencejames083@gmail.com>
2024-05-31 21:34:40 +0200

    Update date latest files

--- app/routes/web.php
+++ app/routes/web.php
@@ -2,6 +2,48 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\homeController;
+
+Route::get('/', [homeController::class, 'index']);
+Route::get('/about', [homeController::class, 'about']);
+Route::get('/contact', [homeController::class, 'contact']);
+
+/*
+URL Query string routing:
+*/
+
+// Route::get('/store', function (){
+
+//     $category = request("category");
+
+//     if(isset($category)){
+
+//         return "You are viewing the store for " . strip_tags($category);
+//     }
+
+//     return 'You are view all content.';
+
+    
+    
+// });
+
+/* URL Parameter routing */
+Route::get('/store/{category?}/{item?}', function ($category = NULL, $item = NULL){
+
+
+
+    if(isset($category)){
+
+        if(isset($item)){
+
+            return "You are viewing the store for $category for $item";
+
+        }
+        return "You are viewing the store for {$category}";
+    }
+
+    return 'You are view all content.';
+
+    
+    
 });
